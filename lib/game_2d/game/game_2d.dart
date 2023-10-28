80f1e3db7bc56ada006dff16f1bd6c4544df64f3
Klaus Magalhaes Balduino <klausmagalhaes@gmail.com>
2023-10-28 01:01:49 -0300

    Working on 2d game movement

--- lib/game_2d/game/game_2d.dart
+++ lib/game_2d/game/game_2d.dart
@@ -14,12 +14,10 @@ class Game2D extends FlameGame with TapCallbacks {
   late double gameHeight;
   late double startY;
   late GameConfig gameConfig;
-
-  static List _getPositionFromIndex(int index, int columns) {
-    final int column = index % columns;
-    final int row = index ~/ columns;
-    return [column, row];
-  }
+  FilledBlock2D? currentBlock;
+  double myDt = 0;
+  int currentIndex = 0;
+  late int maxIndex;
 
   @override
   Future<void> onLoad() async {
@@ -41,14 +39,43 @@ class Game2D extends FlameGame with TapCallbacks {
     gameHeight = blockSize * gameConfig.rows;
     final remainingHeight = size.y - gameHeight;
     startY = (remainingHeight / 2 + CommonStatic.marginSize() / 2);
+    maxIndex = gameConfig.rows * gameConfig.columns;
   }
 
   void addBlock2D(int index) {
+    currentBlock = FilledBlock2D(2, vectorFromIndex(index));
+    add(currentBlock!);
+  }
+
+  Vector2 vectorFromIndex(int index) {
     final reversedIndex = (gameConfig.columns * gameConfig.rows - 1) - index;
     final xy = _getPositionFromIndex(reversedIndex, gameConfig.columns);
     final int x = xy[0];
     final int y = xy[1];
-    add(FilledBlock2D(2, Vector2(startX + x * blockSize, startY + y * blockSize)));
+    return Vector2(startX + x * blockSize, startY + y * blockSize);
+  }
+
+  static List _getPositionFromIndex(int index, int columns) {
+    final int column = index % columns;
+    final int row = index ~/ columns;
+    return [column, row];
+  }
+
+  @override
+  void update(double dt) {
+    myDt = myDt + dt;
+    if(myDt > 0.6) {
+      if(currentBlock != null) {
+        print('moving to index $currentIndex');
+        currentBlock!.position = vectorFromIndex(currentIndex);
+        currentIndex++;
+        if(currentIndex == maxIndex) {
+          currentIndex = 0;
+        }
+      }
+      myDt = 0;
+    }
+    super.update(dt);
   }
 
   /*@override
