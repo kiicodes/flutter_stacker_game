c427a0dff8d2927fdfa3e159dead0beeb610514b
Klaus Magalhaes Balduino <klausmagalhaes@gmail.com>
2023-12-03 01:46:55 -0300

    New config for level stars

--- lib/levels/game_levels.dart
+++ lib/levels/game_levels.dart
@@ -4,18 +4,18 @@ class GameLevels {
   static final List<GameConfig> levels = [
     GameConfig(name: "Level 1", columns: 6, rows: 7, squareQuantity: 3, startMs: 600, lastMs: 300, twoStarsPoints: 220, threeStarsPoints: 260),
     GameConfig(name: "Level 2", columns: 6, rows: 9, squareQuantity: 3, startMs: 600, lastMs: 300, twoStarsPoints: 230, threeStarsPoints: 290),
-    /*GameConfig(name: "Level 3", columns: 6, rows: 12, squareQuantity: 3, startMs: 600, lastMs: 300, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 4", columns: 6, rows: 7, squareQuantity: 3, startMs: 500, lastMs: 200, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 5", columns: 6, rows: 9, squareQuantity: 3, startMs: 500, lastMs: 200, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 6", columns: 6, rows: 12, squareQuantity: 3, startMs: 500, lastMs: 200, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 7", columns: 6, rows: 7, squareQuantity: 3, startMs: 350, lastMs: 80, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 8", columns: 6, rows: 9, squareQuantity: 3, startMs: 350, lastMs: 80, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 9", columns: 6, rows: 12, squareQuantity: 3, startMs: 350, lastMs: 80, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 10", columns: 6, rows: 7, squareQuantity: 3, startMs: 180, lastMs: 20, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 11", columns: 6, rows: 9, squareQuantity: 3, startMs: 180, lastMs: 20, twoStarsPoints: 200, threeStarsPoints: 250),
+    GameConfig(name: "Level 3", columns: 6, rows: 12, squareQuantity: 3, startMs: 600, lastMs: 300, twoStarsPoints: 235, threeStarsPoints: 300),
+    GameConfig(name: "Level 4", columns: 6, rows: 7, squareQuantity: 3, startMs: 500, lastMs: 200, twoStarsPoints: 410, threeStarsPoints: 490),
+    GameConfig(name: "Level 5", columns: 6, rows: 9, squareQuantity: 3, startMs: 500, lastMs: 200, twoStarsPoints: 430, threeStarsPoints: 495),
+    GameConfig(name: "Level 6", columns: 6, rows: 12, squareQuantity: 3, startMs: 500, lastMs: 200, twoStarsPoints: 400, threeStarsPoints: 500),
+    GameConfig(name: "Level 7", columns: 6, rows: 7, squareQuantity: 3, startMs: 350, lastMs: 80, twoStarsPoints: 780, threeStarsPoints: 870),
+    GameConfig(name: "Level 8", columns: 6, rows: 9, squareQuantity: 3, startMs: 350, lastMs: 80, twoStarsPoints: 840, threeStarsPoints: 920),
+    GameConfig(name: "Level 9", columns: 6, rows: 12, squareQuantity: 3, startMs: 350, lastMs: 80, twoStarsPoints: 800, threeStarsPoints: 1000),
+    GameConfig(name: "Level 10", columns: 6, rows: 7, squareQuantity: 3, startMs: 180, lastMs: 20, twoStarsPoints: 1500, threeStarsPoints: 1530),
+    GameConfig(name: "Level 11", columns: 6, rows: 9, squareQuantity: 3, startMs: 180, lastMs: 20, twoStarsPoints: 1600, threeStarsPoints: 1720),
     GameConfig(name: "Level 12", columns: 6, rows: 12, squareQuantity: 3, startMs: 180, lastMs: 20, twoStarsPoints: 200, threeStarsPoints: 250),
     GameConfig(name: "Level 13", columns: 10, rows: 12, squareQuantity: 2, startMs: 180, lastMs: 20, twoStarsPoints: 200, threeStarsPoints: 250),
-    GameConfig(name: "Level 14", columns: 10, rows: 12, squareQuantity: 1, startMs: 180, lastMs: 20, twoStarsPoints: 200, threeStarsPoints: 250),*/
+    GameConfig(name: "Level 14", columns: 10, rows: 12, squareQuantity: 1, startMs: 180, lastMs: 20, twoStarsPoints: 200, threeStarsPoints: 250),
   ];
 
   static int maxEnabledLevel = 0;
